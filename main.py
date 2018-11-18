566889e75576ae735bbd297e26b207eae070bba3
mxz <x_z_meng@qq.com>
2018-11-18 14:50:35 +0800

    test

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
-import numpy as np
 
-print(np.random.random())
+def log(func):
+    def wrapper():
+        print('begin ' + func.__name__)
+        func()
+        print('end ' + func.__name__)
+    return wrapper
+
+@log
+def hello():
+    print("hello")
+
+if __name__ == '__main__':
+    hello()
